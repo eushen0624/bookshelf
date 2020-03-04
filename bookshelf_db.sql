68898df965b8af0a05879483394e7940150803d3
Eushen Jungaya <eushen.6@gmail.com>
2020-03-05 01:43:43 +0800

    Saved sql

--- bookshelf_db.sql
+++ bookshelf_db.sql
@@ -1,10 +1,10 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.1
+-- version 4.9.1
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Generation Time: Mar 02, 2020 at 09:44 AM
--- Server version: 10.4.11-MariaDB
+-- Host: 127.0.0.1
+-- Generation Time: Mar 04, 2020 at 06:42 PM
+-- Server version: 10.4.8-MariaDB
 -- PHP Version: 7.3.14
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
@@ -38,19 +38,21 @@ CREATE TABLE `books` (
   `imgPath` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
   `price` decimal(10,2) NOT NULL,
   `genre_id` bigint(20) UNSIGNED NOT NULL,
-  `transaction_type_id` bigint(20) UNSIGNED NOT NULL
+  `transaction_type_id` bigint(20) UNSIGNED NOT NULL,
+  `book_owner_id` bigint(20) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Dumping data for table `books`
 --
 
-INSERT INTO `books` (`id`, `created_at`, `updated_at`, `name`, `author`, `description`, `imgPath`, `price`, `genre_id`, `transaction_type_id`) VALUES
-(1, NULL, NULL, 'The Handmaid\'s Tale: The Graphic Novel', 'Margaret Atwood', 'The Handmaid\'s Tale, originally published in 1985, is a dystopian novel written by Canadian author Margaret Atwood', 'Random Image', '300.00', 3, 2),
-(2, NULL, NULL, 'Dune', 'Frank Herbert', 'Dune is a 1965 science fiction novel by American author Frank Herbert, originally published as two separate serials in Analog magazine.', 'Random Image', '420.00', 1, 1),
-(3, NULL, NULL, 'Love, Rosie', 'Cecelia Ahern', 'From the bestselling author of PS, I Love You comes a delightfully enchanting novel about what happens when two people who are meant to be together just can\'t seem to get it right.', 'Random Image', '220.00', 2, 1),
-(4, '2020-03-01 16:29:29', '2020-03-01 16:29:29', 'Deadly Little Lies', 'Laurie Faria Stolarz', 'Deadly Little Lies: A Touch Novel. by Laurie Faria Stolarz. Sixteen-year-old Camelia Hammond has survived a horrible nightmare come true.', 'images/1583108969.jpg', '450.00', 3, 1),
-(5, '2020-03-01 22:19:29', '2020-03-01 22:19:29', 'Deadly Little lies', 'Laurie Faria Stolarz', 'Deadly Little Lies: A Touch Novel. by Laurie Faria Stolarz. Sixteen-year-old Camelia Hammond has survived a horrible nightmare come true.', 'images/1583129969.jpg', '600.00', 3, 2);
+INSERT INTO `books` (`id`, `created_at`, `updated_at`, `name`, `author`, `description`, `imgPath`, `price`, `genre_id`, `transaction_type_id`, `book_owner_id`) VALUES
+(1, '2020-03-03 23:39:19', '2020-03-03 23:39:19', 'Shatter Me', 'Tahereh Mafi', 'shatter Me is a young adult dystopian thriller written by Tahereh Mafi, published on November 15, 2011.', 'images/1583307559.jpg', '230.00', 2, 2, 1),
+(2, '2020-03-03 23:40:28', '2020-03-03 23:40:28', 'The Handmaid\'s Tale: The Graphic Novel', 'Margaret Atwood', 'Description: The Handmaid\'s Tale, originally published in 1985, is a dystopian novel written by Canadian author Margaret Atwood', 'images/1583307628.jpg', '344.00', 6, 1, 1),
+(3, '2020-03-04 09:07:25', '2020-03-04 09:07:25', 'Throne of Glass', 'Sarah J. Maas', 'Throne of Glass is a young adult high fantasy novel series by American author Sarah J. Maas, beginning with the novel of the same name, released in August 2012.', 'images/1583341645.jpg', '1300.00', 5, 1, 1),
+(4, '2020-03-04 09:08:58', '2020-03-04 09:08:58', 'The Paris Wife', 'Paula McLain', 'The Paris Wife is a 2011 historical fiction novel by Paula McLain which became a New York Times Bestseller.', 'images/1583341738.jpg', '590.00', 3, 2, 1),
+(5, '2020-03-04 09:11:50', '2020-03-04 09:11:50', 'Dune', 'Frank Herbert', 'Dune is a 1965 science fiction novel by American author Frank Herbert, originally published as two separate serials in Analog magazine.', 'images/1583341910.jpeg', '560.00', 1, 2, 1),
+(6, '2020-03-04 09:13:07', '2020-03-04 09:13:07', 'Love, Rosie', 'Cecelia Ahern', 'Rosie and Alex have been friends since childhood and cannot imagine themselves as a couple.', 'images/1583341987.jpg', '620.00', 2, 1, 1);
 
 -- --------------------------------------------------------
 
@@ -88,7 +90,7 @@ INSERT INTO `genres` (`id`, `created_at`, `updated_at`, `name`) VALUES
 (1, NULL, NULL, 'Sci-fi'),
 (2, NULL, NULL, 'Romance'),
 (3, NULL, NULL, 'Fiction'),
-(4, NULL, NULL, 'Thiller'),
+(4, NULL, NULL, 'Thriller'),
 (5, NULL, NULL, 'Fantasy'),
 (6, NULL, NULL, 'Drama');
 
@@ -120,7 +122,8 @@ INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 (9, '2020_02_27_030757_create_transaction_types_table', 1),
 (10, '2020_02_27_030816_create_books_table', 1),
 (11, '2020_02_27_030906_create_orders_table', 1),
-(12, '2020_02_27_031034_add_role_to_users', 1);
+(12, '2020_02_27_031034_add_role_to_users', 1),
+(13, '2020_02_29_190103_add_details_to_user', 1);
 
 -- --------------------------------------------------------
 
@@ -140,6 +143,14 @@ CREATE TABLE `orders` (
   `transaction_id` bigint(20) UNSIGNED NOT NULL,
   `process_id` bigint(20) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `orders`
+--
+
+INSERT INTO `orders` (`id`, `created_at`, `updated_at`, `total`, `user_owner_id`, `user_buyer_id`, `payment_id`, `status_id`, `transaction_id`, `process_id`) VALUES
+(3, '2020-03-04 00:33:48', '2020-03-04 00:33:48', '344.00', 1, 1, 1, 1, 1, 1),
+(4, '2020-03-04 08:33:42', '2020-03-04 08:33:42', '230.00', 1, 1, 1, 1, 2, 1);
 
 -- --------------------------------------------------------
 
@@ -166,6 +177,13 @@ CREATE TABLE `payments` (
   `name` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+--
+-- Dumping data for table `payments`
+--
+
+INSERT INTO `payments` (`id`, `created_at`, `updated_at`, `name`) VALUES
+(1, NULL, NULL, 'Pay via Bank');
+
 -- --------------------------------------------------------
 
 --
@@ -179,6 +197,13 @@ CREATE TABLE `processes` (
   `name` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+--
+-- Dumping data for table `processes`
+--
+
+INSERT INTO `processes` (`id`, `created_at`, `updated_at`, `name`) VALUES
+(1, NULL, NULL, 'Meet Up');
+
 -- --------------------------------------------------------
 
 --
@@ -191,6 +216,14 @@ CREATE TABLE `roles` (
   `updated_at` timestamp NULL DEFAULT NULL,
   `name` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `roles`
+--
+
+INSERT INTO `roles` (`id`, `created_at`, `updated_at`, `name`) VALUES
+(1, NULL, NULL, 'Admin'),
+(2, NULL, NULL, 'User');
 
 -- --------------------------------------------------------
 
@@ -213,7 +246,8 @@ INSERT INTO `statuses` (`id`, `created_at`, `updated_at`, `name`) VALUES
 (1, NULL, NULL, 'Pending'),
 (2, NULL, NULL, 'In Shelf'),
 (3, NULL, NULL, 'Out Shelf'),
-(4, NULL, NULL, 'Cancelled');
+(4, NULL, NULL, 'Cancelled By Owner'),
+(5, NULL, NULL, 'Cancelled By Buyer');
 
 -- --------------------------------------------------------
 
@@ -251,8 +285,19 @@ CREATE TABLE `users` (
   `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
-  `role_id` bigint(20) UNSIGNED NOT NULL DEFAULT 2
+  `role_id` bigint(20) UNSIGNED NOT NULL DEFAULT 2,
+  `address` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `phone` int(11) DEFAULT NULL,
+  `avatar` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT 'default.jpg'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`, `role_id`, `address`, `phone`, `avatar`) VALUES
+(1, 'Brenda Mage', 'user@user.com', NULL, '$2y$10$pS8HddlEAivRFMIQoYVwmOk3Gr7igG2aH/a2SIOER0H6pohUOXk/G', NULL, '2020-03-03 23:38:21', '2020-03-03 23:38:21', 2, 'Makati City', 234792385, 'default.jpg'),
+(2, 'Admin', 'admin@admin.com', NULL, '$2y$10$tdqibplqyFGsMx6t/tW0IuYkZJdEg6PYFjnEObF7xeZPsbVhIumnu', NULL, '2020-03-04 00:47:20', '2020-03-04 00:47:20', 1, 'Quezon City', 74923857, 'default.jpg');
 
 --
 -- Indexes for dumped tables
@@ -264,7 +309,8 @@ CREATE TABLE `users` (
 ALTER TABLE `books`
   ADD PRIMARY KEY (`id`),
   ADD KEY `books_genre_id_foreign` (`genre_id`),
-  ADD KEY `books_transaction_type_id_foreign` (`transaction_type_id`);
+  ADD KEY `books_transaction_type_id_foreign` (`transaction_type_id`),
+  ADD KEY `books_book_owner_id_foreign` (`book_owner_id`);
 
 --
 -- Indexes for table `failed_jobs`
@@ -348,7 +394,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `books`
 --
 ALTER TABLE `books`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `failed_jobs`
@@ -366,37 +412,37 @@ ALTER TABLE `genres`
 -- AUTO_INCREMENT for table `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT for table `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `payments`
 --
 ALTER TABLE `payments`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `processes`
 --
 ALTER TABLE `processes`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `roles`
 --
 ALTER TABLE `roles`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `statuses`
 --
 ALTER TABLE `statuses`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `transaction_types`
@@ -408,7 +454,7 @@ ALTER TABLE `transaction_types`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- Constraints for dumped tables
@@ -418,6 +464,7 @@ ALTER TABLE `users`
 -- Constraints for table `books`
 --
 ALTER TABLE `books`
+  ADD CONSTRAINT `books_book_owner_id_foreign` FOREIGN KEY (`book_owner_id`) REFERENCES `users` (`id`) ON UPDATE CASCADE,
   ADD CONSTRAINT `books_genre_id_foreign` FOREIGN KEY (`genre_id`) REFERENCES `genres` (`id`) ON UPDATE CASCADE,
   ADD CONSTRAINT `books_transaction_type_id_foreign` FOREIGN KEY (`transaction_type_id`) REFERENCES `transaction_types` (`id`) ON UPDATE CASCADE;
 
